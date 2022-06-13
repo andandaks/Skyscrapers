4926b5159cefb34dff2d4030906a21d79917c9bc
andandaks <105543015+andandaks@users.noreply.github.com>
2022-06-13 23:08:22 +0300

    Update telega.py

--- telega.py
+++ telega.py
@@ -2,6 +2,7 @@ import telebot
 from PIL import Image, ImageDraw
 import requests
 from bs4 import BeautifulSoup as bs
+import urllib.request
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
